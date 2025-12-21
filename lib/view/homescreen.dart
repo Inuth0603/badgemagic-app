c69cb7095eb9e208fb1abba42646ce12c1e4a098
Divyam Agarwal <iitdivyamagarwal@gmail.com>
2025-12-21 23:45:01 +0530

    fix: restrict unsupported emoji input in badge creator

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -264,6 +264,14 @@ class _HomeScreenState extends State<HomeScreen>
                               borderRadius: BorderRadius.circular(10.r),
                               elevation: 4,
                               child: ExtendedTextField(
+                               // --- START OF FIX ---
+                              // This blocks Emojis and unsupported symbols from being typed
+                              inputFormatters: [
+                                FilteringTextInputFormatter.deny(
+                                  RegExp(r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'),
+                                ),
+                              ],
+                              // --- END OF FIX ---
                                 onChanged: (value) {},
                                 controller: inlineimagecontroller,
                                 specialTextSpanBuilder: ImageBuilder(),
